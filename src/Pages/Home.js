b0f62854d4662f6adc29d8c6d7b6b2269112b4ba
wello224 <74426146+MohamedWalied-dash@users.noreply.github.com>
2022-12-19 21:32:37 +0200

    finsh home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from '../Components/card'
 import View from '../Components/view'
-import Footer from '../Components/footer'
 import Box from '../Components/box'
 const Home=()=>{
     return(
